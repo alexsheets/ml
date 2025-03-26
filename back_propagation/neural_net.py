d5aa8ff3997daeca1cb7c02599d19185782c67b8
Alex Sheets <asheet3@lsu.edu>
2025-03-26 14:25:22 -0500

    create classes, start neuron logic/forward pass ideas

--- back_propagation/neural_net.py
+++ back_propagation/neural_net.py
@@ -19,6 +19,49 @@ import math
 
 # initial weights, biases, training inputs and outputs: https://mattmazur.com/wp-content/uploads/2018/03/neural_network-9.png
 
+# Wikipedia article on Backpropagation
+#   http://en.wikipedia.org/wiki/Backpropagation#Finding_the_derivative_of_the_error
+# The Back Propagation Algorithm
+#   https://www4.rgu.ac.uk/files/chapter3%20-%20bp.pdf
 
 class BackPropagation:
     LEARNING_RATE = 0.5
+
+
+class NeuronLayer:
+    def __init__(self, num, bias):
+        self.bias = bias
+        self.neurons = []
+        for i in range(num):
+            self.neurons.append(Neuron(self.bias))
+
+
+class Neuron:
+    def __init__(self, bias):
+        self.bias = bias
+        self.weights = []
+
+    # forward pass: 
+    # figure out total net input to each hidden layer neuron
+    # squash the total net input using activation (logistic here) function
+    # repeat process with output layer neurons
+
+    def net_input(self):
+        # simply calculate total input to this neuron and return
+        # include bias in return value
+        net = 0
+        for i in self.inputs:
+            net += self.inputs[i] * self.weights[i]
+        return net + self.bias
+    
+    # squash function to use on output of neuron
+    def squash(self, net_input):
+        # formula of form 1 / 1 + e^-net
+        return 1 / (1 + math.exp(-net_input))
+    
+    def calc_output(self, inputs):
+        self.inputs = inputs
+        # repeat the process of calculating net input and squashing for each
+        # using the output from hidden layer neurons as inputs
+        self.output = self.squash(self.net_input())
+        
